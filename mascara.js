82a22a223547de2f211ba5c26da09a2aaa25fd12
Aline Dias <aline.meldias@gmail.com>
2017-11-06 18:47:29 -0200

    Adicionando máscara de CPF

--- mascara.js
+++ mascara.js
@@ -3,6 +3,9 @@
 var telefone = document.querySelector('[data-mapping="telefone"] input');
 telefone.id = 'telefone';
 
+var cpf = document.querySelector('[data-mapping="cpf"] input');
+cpf.id = 'cpf';
+
 
 /* Máscaras ER */
 function mascara(o,f){
@@ -15,12 +18,22 @@ function execmascara(){
 }
 
 // Aplicando regras da máscara com regex
+
+// Telefone
 function mascaraTel(v){
     v=v.replace(/\D/g,"");                  //Remove tudo o que não é dígito
     v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
     v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
     return v;
 }
+// CPF
+function mascaraCpf(v){
+    v=v.replace(/\D/g,"");                                          // Remove tudo o que não é dígito
+    v=v.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/,"$1.$2.$3-$4");    // Coloca formatação CPF
+    return v;
+}
+
+
 // Atalho para seleção de elemento por ID
 function id(elemento){
 	return document.getElementById(elemento);
@@ -28,8 +41,13 @@ function id(elemento){
 
 // Executa função de máscara do ID informado
 window.onload = function(){
+    
 	id('telefone').onkeyup = function(){
 		mascara(this, mascaraTel);
+    }
+    id('cpf').onkeyup = function(){
+        mascara(this, mascaraCpf);
 	}
 }
 
+console.log(cpf.value);
